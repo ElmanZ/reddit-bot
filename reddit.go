4dd1377e6ec57a92c99d3ba4442e8bdec7b1386a
Elman Zakirov <elmanzakirov@gmail.com>
2021-12-13 21:18:48 +0300

    added reddit api

---
+++ reddit.go
@@ -0,0 +1,56 @@
+package reddit
+
+import (
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+)
+
+type Item struct {
+	Title string `json:"title"`
+	URL   string `json:"url"`
+}
+
+type Response struct {
+	Data struct {
+		Childern []struct {
+			Data Item
+		}
+	}
+}
+
+func Get(reddit string) ([]string, error) {
+	req, err := http.NewRequest("GET", reddit, nil)
+	if err != nil {
+		return nil, err
+	}
+	client := &http.Client{}
+
+	//UserAgent retrieves and presents web content for end users e.g. web browser
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:88.0) Gecko/20100101 Firefox/88.0")
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New(resp.Status)
+	}
+
+	r := new(Response)
+	err = json.NewDecoder(resp.Body).Decode(r)
+	if err != nil {
+		return nil, err
+	}
+
+	//grabing values from response and appending them onto slice
+	var items []string
+	for _, child := range r.Data.Childern {
+		items = append(items, "\nURL: ", child.Data.URL, "\nTitle: ", child.Data.Title, "\n")
+		//for logs
+		log.Printf("\nURL: %s, \nTitle: %s", child.Data.URL, child.Data.Title)
+	}
+	return items, err
+}
